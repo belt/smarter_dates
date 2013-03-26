b9a8af5e9f86d45531e301218000506759cf1309
Paul Belt <saprb@channing.harvard.edu>
2013-03-26 14:30:15 -0400

    .to_datetime, .to_date and .to_time for String

--- lib/smarter_dates.rb
+++ lib/smarter_dates.rb
@@ -1,5 +1,6 @@
 require File.join('smarter_dates', 'version')
 require File.join('smarter_dates', 'parser')
+require File.join('smarter_dates', 'chronic_strings')
 if defined? ActiveModel
   require File.join('smarter_dates', 'chronic_parsable_validator')
 end
