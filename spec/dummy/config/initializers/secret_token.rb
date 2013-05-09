c336c55b38e71fe6c35a9419091ab44fb5885b93
Paul Belt <saprb@channing.harvard.edu>
2013-05-09 01:45:44 -0400

    dummy-app for testing rails

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'a00dbc3165d8755822c7ee14c79866117462eaacbdd96259c68c52d5db18fe60e333ddf6eb8a865a253e7b94b6411afa967730d2f988a438279a9c50f66291ee'
---
+++ spec/dummy/config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  #mount SmarterDates::Engine => "/smarter_dates"
+end
